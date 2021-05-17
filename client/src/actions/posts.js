d06b28fc1317be78a535ed272975880d32d40f63
nadira arasya <nadira.arasya.tif18@polban.ac.id>
2021-05-17 10:25:51 +0700

    first deploy

---
+++ client/src/actions/posts.js
@@ -0,0 +1,55 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import * as api from '../api';
+
+
+//Actions creators
+export const getPosts = () => async (dispatch) => {
+    try{
+        const { data } = await api.fetchPosts();
+        console.log(data);
+        dispatch({ type: FETCH_ALL, payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+export const createPost = (post) => async(dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+        console.log(post);
+        dispatch({ type: CREATE, payload: data })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) =>{
+    try {
+        const { data } = await api.updatePost(id,post);
+
+        dispatch({ type: UPDATE, payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: DELETE, payload: id})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = (id) => async (dispatch) =>{
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type: UPDATE, payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+}
